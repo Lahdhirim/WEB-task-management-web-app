ba4d43069feaeed811cbbb36da28eb3448867566
Lahdhiri Moetez <moetez.lahdhiri@student-cs.fr>
2025-07-06 17:27:11 +0200

    push cancel task button

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
 import { DUMMY_TASKS } from './dummy-tasks';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
@@ -13,6 +14,7 @@ export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
   tasks = DUMMY_TASKS;
+  isAddingTask = false;
 
   get selectedUserTasks() {
     return this.tasks.filter(task => task.userId === this.userId);
@@ -20,6 +22,14 @@ export class TasksComponent {
 
   deleteTask(taskId: string) {
     this.tasks = this.tasks.filter(task => task.id !== taskId);
+  }
+
+  addNewTask() {
+    this.isAddingTask = true;
+  }
+
+  cancelAddTask() {
+    this.isAddingTask = false;
   }
 
 }
